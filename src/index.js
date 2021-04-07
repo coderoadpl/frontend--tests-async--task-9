aceeb5cdad2ea633914744e9450167b3135b6376
Mateusz Choma <kontakt@coderoad.pl>
2021-04-07 17:08:31 +0200

    Hello world added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>React App</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+console.log('Hello CodeRoad!')
