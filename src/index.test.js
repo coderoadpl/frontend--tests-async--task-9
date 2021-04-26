2c5113b69b5dadf57e63b6b7df256833a994c5cf
Mateusz Choma <kontakt@coderoad.pl>
2021-04-26 17:12:10 +0200

    Basic tests added

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,27 @@
-it('should pass', () => {
-    expect(true).toBe(true)
+import { createSquare, startFalling, moveDown } from './fallingSquare'
+
+it('should move element down 10px', () => {
+    const div = document.createElement('div')
+    div.style.top = '10px'
+    
+    moveDown(div)
+    
+    expect(div.style.top).toBe('20px')
+})
+
+it('should move element down 100px', () => {
+    const div = document.createElement('div')
+    div.style.top = '10px'
+    
+    moveDown(div, 100)
+    
+    expect(div.style.top).toBe('110px')
+})
+
+it('should create fixed square', () => {
+    const square = createSquare()
+    
+    expect(square.style.position).toBe('fixed')
+    expect(square.style.top).toBe('0px')
+    expect(square.style.left).toBe('0px')
 })
