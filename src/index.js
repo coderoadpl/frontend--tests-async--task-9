970e014428a6877116211ad806968570ad1e2e43
Mateusz Choma <kontakt@coderoad.pl>
2021-04-26 17:01:20 +0200

    `createSquare` added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello CodeRoad!')
+const createSquare = () => {
+    const square = document.createElement('div')
+
+    square.style.backgroundColor = 'red'
+    square.style.width = '100px'
+    square.style.height = '100px'
+    square.style.position = 'fixed'
+    square.style.top = '0px'
+    square.style.left = '0px'
+
+    return square
+}
+
+document.body.appendChild(createSquare())
