7efea64ca63ccf7d5ec11d08f1a43438de013155
Mateusz Choma <kontakt@coderoad.pl>
2021-04-26 17:06:36 +0200

    Falling square done

--- src/index.js
+++ src/index.js
@@ -11,4 +11,22 @@ const createSquare = () => {
     return square
 }
 
-document.body.appendChild(createSquare())
+const moveDown = (element, step = 10) => {
+    const currentTop = Number(element.style.top.replace('px', ''))
+    const newTop = currentTop + step
+    element.style.top = newTop + 'px'
+    return element
+}
+
+const startFalling = (element) => {
+    setInterval(
+        () => moveDown(element),
+        1 * 1000
+    )
+}
+
+const square = createSquare()
+
+document.body.appendChild(square)
+
+startFalling(square)
